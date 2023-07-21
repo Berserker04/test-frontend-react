b213454e6b3cf90afd24aea60e8282bd3b82fef3
Berserker04 <xcarlosdesarrolladorx@gmail.com>
2023-07-21 15:50:57 -0500

    feat: styles for navbar and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Styles from "./_Styles.module.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={`${Styles.footer} bg-dark text-center text-white`}>
+      <div className="text-center p-3">
+        © 2023 Copyright:
+        <a className="text-white" href="https://chernandezdev.online/">
+          Berserker04
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
